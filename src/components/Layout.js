4221fd8184a109e1291455f2925987ac17dd2431
zswalker <zswalker@my.waketech.edu>
2024-01-31 18:00:31 -0500

    add initial page layout

---
+++ src/components/Layout.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import { Helmet } from "react-helmet";
+import { Container } from "@mui/material";
+import "./Layout.css";
+import { useTheme } from "@emotion/react";
+import "../index.css";
+
+const Layout = ({ title, header, footer, children }) => {
+  const theme = useTheme();
+  const [isLoading, setIsLoading] = useState(true);
+
+  // This useEffect will run once when the component mounts
+  useEffect(() => {
+    // Simulate some delay for demonstration purposes
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+
+    // Cleanup function to cancel the timeout in case the component unmounts before the timeout finishes
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
+  return (
+    <div
+      className=""
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: theme.palette.backgroundColor,
+      }}
+    >
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
+      <Header className="flex flex-none">{header}</Header>
+
+      <Container
+        className="flex flex-1 flex-col min-h-max overflow-y-scroll min-w-full scroll-container2"
+        sx={{
+          paddingLeft: { xs: 0, sm: 2, md: 4 },
+          paddingRight: { xs: 0, sm: 2, md: 4 },
+        }}
+      >
+        {isLoading ? (
+          <div className="flex items-center">
+            <div class="lds-ring">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        ) : (
+          <div>{children}</div>
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default Layout;
