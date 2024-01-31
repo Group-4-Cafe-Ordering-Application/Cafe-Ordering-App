04f70e946e353cadbf02659686afeb43109d59f7
zswalker <zswalker@my.waketech.edu>
2024-01-31 17:55:17 -0500

    add SVGs to header

---
+++ src/svgs/CartSVG.js
@@ -0,0 +1,42 @@
+import { useTheme } from "@emotion/react";
+
+const CartSVG = ({ isMenuStyle }) => {
+  const theme = useTheme();
+
+  return (
+    <div className="popup-container flex items-evenly">
+      {isMenuStyle ? null : <div className="popup-content">View Cart</div>}
+      <div
+        className="flex rounded-xl justify-center"
+        style={{
+          backgroundColor: theme.palette.secondary.main,
+          height: isMenuStyle ? "36px" : "48px",
+          width: isMenuStyle ? "36px" : "48px",
+        }}
+      >
+        <svg
+          className=" m-auto"
+          style={{
+            width: isMenuStyle ? "36px" : "40px",
+            height: isMenuStyle ? "36px" : "48px",
+            fill: theme.palette.secondary.main,
+          }}
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          xmlSpace="preserve"
+        >
+          <path
+            d="M7.2998 5H22L20 12H8.37675M21 16H9L7 3H4M4 8H2M5 11H2M6 14H2M10 20C10 20.5523 9.55228 21 9 21C8.44772 21 8 20.5523 8 20C8 19.4477 8.44772 19 9 19C9.55228 19 10 19.4477 10 20ZM21 20C21 20.5523 20.5523 21 20 21C19.4477 21 19 20.5523 19 20C19 19.4477 19.4477 19 20 19C20.5523 19 21 19.4477 21 20Z"
+            stroke={theme.palette.secondary.text}
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default CartSVG;
---
+++ src/svgs/HomeSVG.js
@@ -0,0 +1,54 @@
+import { useTheme } from "@emotion/react";
+
+const HomeSVG = ({ isMenuStyle }) => {
+  const theme = useTheme();
+
+  return (
+    <div className="popup-container flex items-evenly">
+      {isMenuStyle ? null : <div className="popup-content">Home</div>}
+      <div
+        className="flex rounded-xl justify-center"
+        style={{
+          backgroundColor: theme.palette.secondary.main,
+          height: isMenuStyle ? "36px" : "48px",
+          width: isMenuStyle ? "36px" : "48px",
+        }}
+      >
+        <svg
+          className=" m-auto"
+          style={{
+            width: isMenuStyle ? "36px" : "40px",
+            height: isMenuStyle ? "36px" : "48px",
+          }}
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 -0.5 21 21"
+          xmlSpace="preserve"
+        >
+          <g
+            id="Page-1"
+            stroke="none"
+            stroke-width="1"
+            style={{ fill: theme.palette.secondary.main }}
+            fill-rule="evenodd"
+          >
+            <g
+              id="Dribbble-Light-Preview"
+              transform="translate(-419.000000, -720.000000)"
+              style={{ fill: theme.palette.secondary.text }}
+            >
+              <g id="icons" transform="translate(56.000000, 160.000000)">
+                <path
+                  d="M379.79996,578 L376.649968,578 L376.649968,574 L370.349983,574 L370.349983,578 L367.19999,578 L367.19999,568.813 L373.489475,562.823 L379.79996,568.832 L379.79996,578 Z M381.899955,568.004 L381.899955,568 L381.899955,568 L373.502075,560 L363,569.992 L364.481546,571.406 L365.099995,570.813 L365.099995,580 L372.449978,580 L372.449978,576 L374.549973,576 L374.549973,580 L381.899955,580 L381.899955,579.997 L381.899955,570.832 L382.514204,571.416 L384.001,570.002 L381.899955,568.004 Z"
+                  id="home-[#1391]"
+                ></path>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSVG;
