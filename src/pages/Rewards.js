8dd6a61561bc30209337e21d7041e6ef4fff9bee
zswalker <zswalker@my.waketech.edu>
2024-04-19 21:13:11 -0400

    adjust wording and styling

--- src/pages/Rewards.js
+++ src/pages/Rewards.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useTheme } from "@emotion/react";
 
 function Rewards() {
+  const theme = useTheme();
   return (
     <Layout title="Cafe Rewards">
-      <div>Rewards Page</div>
+      <div
+        className="flex justify-center items-center h-screen text-2xl"
+        style={{ color: theme.palette.primary.text }}
+      >
+        Rewards program coming soon!
+      </div>
     </Layout>
   );
 }
